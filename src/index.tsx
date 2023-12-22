566311d20a5349d74fcb48c191af23a7502cddc2
pgagrwl <pgagrwl@gmail.com>
2023-12-22 17:39:24 +0530

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+// import App2 from './App2';
+import LoginComponent from "./LoginComponent";
+import LoginService from "./services/LoginService";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const loginService = new LoginService();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const setToken = (token: string) => {
+  console.log(`received the token ${token}`);
+};
+
+root.render(
+  // <App2 />
+  <LoginComponent loginService={loginService} setToken={setToken} />
+);
